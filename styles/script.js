dd50074b2801b54052306b4bd9e51ee4e6ed857a
Leonard101k <leonardkachumi@gmail.com>
2024-03-21 19:22:03 +0200

    second commit

--- styles/script.js
+++ styles/script.js
@@ -1,31 +1,25 @@
-const catImagesContainer = document.getElementById('catImages');
+document.addEventListener("DOMContentLoaded", function() {
+    const apiKey = "live_0aBxoTzHeQo58VwyojmAm0sbOUPrFKiIz7Klmz2Ybg7vQitFyb2N9HLA01bf0sII";
+    const catGallery = document.querySelector(".cat-gallery");
 
-fetch('https://api.thecatapi.com/v1/images/search?limit=10', {
-        headers: {
-            'x-api-key': 'live_0aBxoTzHeQo58VwyojmAm0sbOUPrFKiIz7Klmz2Ybg7vQitFyb2N9HLA01bf0sII'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(cat => {
-            const img = document.createElement('img');
-            img.src = cat.url;
-            catImagesContainer.appendChild(img);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching cat images:', error);
-    });
-const funFacts = [
-    "Cats sleep for about 70% of their lives.",
-    "The average cat can jump five times its height in a single bound.",
-    "Cats have a specialized collarbone that allows them to always land on their feet.",
-    "Cats have a strong aversion to anything citrus."
-];
-
-const funFactsList = document.getElementById('funFacts');
-funFacts.forEach(fact => {
-    const li = document.createElement('li');
-    li.textContent = fact;
-    funFactsList.appendChild(li);
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=10&page=1&order=Desc`, {
+            headers: {
+                "x-api-key": apiKey
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(cat => {
+                const catImage = document.createElement("div");
+                catImage.classList.add("cat-image");
+                catImage.innerHTML = `<img src="${cat.url}" alt="Cat">`;
+                catGallery.appendChild(catImage);
+            });
+        })
+        .catch(error => console.error("Error fetching cat images:", error));
 });
